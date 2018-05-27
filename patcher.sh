7daecccc4eeb92c1e74db31a901a95caf073c3a2
Mustang-ssc <lilhali6@gmail.com>
2018-05-27 12:11:29 +0000

    introduce RR dedicated patcher

--- patcher.sh
+++ patcher.sh
@@ -4,50 +4,21 @@
 
 ROM_TREE=$PWD
 PATCHER_PATH=$ROM_TREE/patcher
-SULTAN=$PATCHER_PATH/sultan
+MUSTANG=$PATCHER_PATH/mustang
 CUSTOM=$PATCHER_PATH/custom
 
 # Clean up first
 cd $ROM_TREE/build
-git clean -f -d && git reset --hard
-cd $ROM_TREE/frameworks/av
-git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
-git clean -f -d && git reset --hard
-cd $ROM_TREE/hardware/qcom/display-caf/msm8974
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Dialer
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Gallery2
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/LockClock
 git clean -f -d && git reset --hard
 cd $ROM_TREE/packages/apps/Settings
 git clean -f -d && git reset --hard
-cd $ROM_TREE/system/core
-git clean -f -d && git reset --hard
-cd $ROM_TREE/vendor/cm
-git clean -f -d && git reset --hard
+
 
 cd $ROM_TREE
 
-### Sultan's patches
-patch -d build					-p1 -s -N --no-backup-if-mismatch < $SULTAN/build0.patch
-patch -d frameworks/av				-p1 -s -N --no-backup-if-mismatch < $SULTAN/frameworks-av0.patch
-patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SULTAN/frameworks-base0.patch
-patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SULTAN/frameworks-base1.patch
-patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SULTAN/frameworks-base2.patch
-patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SULTAN/frameworks-base3.patch
-patch -d hardware/qcom/display-caf/msm8974	-p1 -s -N --no-backup-if-mismatch < $SULTAN/hardware-qcom-display-caf-msm89740.patch
-patch -d packages/apps/Dialer			-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-Dialer0.patch
-patch -d packages/apps/LockClock		-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-LockClock0.patch
-patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-Settings0.patch
-patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $SULTAN/system-core0.patch
-patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $SULTAN/system-core1.patch
-patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $SULTAN/vendor-cm0.patch
-patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $SULTAN/vendor-cm1.patch
+### RR-MUSTANG's patches
+patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $MUSTANG/frameworks-base0.patch
+patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $MUSTANG/resurrection_packages_apps_settings0.patch
 
 ### Custom patches
-patch -d packages/apps/Gallery2			-p1 -s -N --no-backup-if-mismatch < $CUSTOM/packages-apps-Gallery20.patch
-patch -d packages/apps/Gallery2			-p1 -s -N --no-backup-if-mismatch < $CUSTOM/packages-apps-Gallery21.patch
-patch -d packages/apps/Gallery2			-p1 -s -N --no-backup-if-mismatch < $CUSTOM/packages-apps-Gallery22.patch
--- unpatcher.sh
+++ unpatcher.sh
@@ -4,24 +4,10 @@ ROM_TREE=$PWD
 
 # Clean up
 cd $ROM_TREE/build
-git clean -f -d && git reset --hard
-cd $ROM_TREE/frameworks/av
+cd $ROM_TREE/packages/apps/Settings
 git clean -f -d && git reset --hard
 cd $ROM_TREE/frameworks/base
 git clean -f -d && git reset --hard
-cd $ROM_TREE/hardware/qcom/display-caf/msm8974
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Dialer
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Gallery2
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/LockClock
-git clean -f -d && git reset --hard
-cd $ROM_TREE/packages/apps/Settings
-git clean -f -d && git reset --hard
-cd $ROM_TREE/system/core
-git clean -f -d && git reset --hard
-cd $ROM_TREE/vendor/cm
-git clean -f -d && git reset --hard
+
 
 cd $ROM_TREE
